92c611e03d0c5b4f4dc3ff079fe41890ba56e65d
Essie006 <esthernyamweya12@gmail.com>
2026-08-05 19:49:03 +0300

    Improve PWA app installation

--- hub/static/hub/sw.js
+++ hub/static/hub/sw.js
@@ -1,8 +1,10 @@
-const CACHE_NAME = "experience-hub-v1";
+const CACHE_NAME = "experience-hub-v2";
 
 const FILES_TO_CACHE = [
     "/",
-    "/static/hub/manifest.json"
+    "/static/hub/manifest.json",
+    "/static/hub/icons/icon-192.png",
+    "/static/hub/icons/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
@@ -10,6 +12,18 @@ self.addEventListener("install", event => {
         caches.open(CACHE_NAME).then(cache => {
             return cache.addAll(FILES_TO_CACHE);
         })
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        )
     );
 });
 
