a38c6d5e41a6a1cb0a051d3203ad2d38d4375e4e
Essie006 <esthernyamweya12@gmail.com>
2026-08-09 21:16:47 +0300

    Make Experience Hub installable as an app

---
+++ hub/static/hub/js/service-worker.js
@@ -0,0 +1,37 @@
+﻿const CACHE_NAME = "experience-hub-v1";
+
+const APP_FILES = [
+    "/",
+    "/booking/",
+    "/about/",
+    "/events/",
+    "/experiences/",
+    "/collections/",
+    "/contact/"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(APP_FILES))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        )
+    );
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
+    );
+});
